f529c99b4ec2f27f1f6d03d8c5569c2911261fea
marinyordanov1 <extremo966@gmail.com>
2022-06-22 16:25:13 +0300

    Update
    pushing a middleware delete

--- models/campground.js
+++ models/campground.js
@@ -1,5 +1,6 @@
 const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
+const Review = require('./review');
 
 const CampgroundShema = new Schema({
   title: String,
@@ -13,6 +14,16 @@ const CampgroundShema = new Schema({
       ref: 'Review',
     },
   ],
+});
+
+CampgroundShema.post('findOneAndDelete', async function (doc) {
+  if (doc) {
+    await Review.deleteMany({
+      _id: {
+        $in: doc.reviews,
+      },
+    });
+  }
 });
 
 module.exports = mongoose.model('Campground', CampgroundShema);
