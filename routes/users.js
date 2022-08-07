d3ae16c05004471bf58207d6d707deef6232a6a9
marinyordanov1 <extremo966@gmail.com>
2022-08-07 10:52:47 +0300

    show Login/Logout condition

--- routes/users.js
+++ routes/users.js
@@ -39,4 +39,14 @@ router.post(
   }
 );
 
+router.get('/logout', (req, res, next) => {
+  req.logout(function (err) {
+    if (err) {
+      return next(err);
+    }
+    req.flash('success', 'Goodbye!');
+    res.redirect('/campgrounds');
+  });
+});
+
 module.exports = router;
