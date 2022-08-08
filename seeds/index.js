85380958bb64782f912ec689623ca54f3fdd9b22
marinyordanov1 <extremo966@gmail.com>
2022-08-08 12:20:19 +0300

    Added author

--- seeds/index.js
+++ seeds/index.js
@@ -23,6 +23,7 @@ const seedDB = async () => {
     const random1000 = Math.floor(Math.random() * 1000);
     const price = Math.floor(Math.random() * 20) + 10;
     const camp = new Campground({
+      author: '62ee74746b63b92608a6d3b5',
       location: `${cities[random1000].city}, ${cities[random1000].state}`,
       title: `${sample(descriptors)} ${sample(places)}`,
       image: 'http://source.unsplash.com/collection/483251',
